7225452212c23ab53cd54f1edc6c0487d58a56e2
Jürgen Viljaste <j.viljaste@gmail.com>
2014-11-17 22:59:14 +0200

    Update

--- src/build/modules/adminer/manifests/init.pp
+++ src/build/modules/adminer/manifests/init.pp
@@ -3,6 +3,6 @@ class adminer {
 
   file { '/var/www/index.php':
     ensure => present,
-    source => 'puppet:///modules/adminer/var/www/adminer-4.1.0.php'
+    source => 'puppet:///modules/adminer/var/www/index.php'
   }
 }
