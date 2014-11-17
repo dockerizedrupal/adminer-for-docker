bdc2da49668ff25962ea01a2368e6f2845aea1fd
Jürgen Viljaste <j.viljaste@gmail.com>
2014-11-17 22:06:57 +0200

    Update

--- src/build/modules/adminer/manifests/httpd/supervisor.pp
+++ src/build/modules/adminer/manifests/httpd/supervisor.pp
@@ -1,6 +1,6 @@
 class adminer::httpd::supervisor {
   file { '/etc/supervisor/conf.d/httpd.conf':
     ensure => present,
-    source => 'puppet:///modules/httpd/etc/supervisor/conf.d/httpd.conf'
+    source => 'puppet:///modules/adminer/etc/supervisor/conf.d/httpd.conf'
   }
 }
