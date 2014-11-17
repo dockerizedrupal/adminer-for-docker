88880af333504c75200d404ae96d2987623f832e
Jürgen Viljaste <j.viljaste@gmail.com>
2014-11-17 21:53:12 +0200

    Update

---
+++ src/build/modules/adminer/manifests/httpd/supervisor.pp
@@ -0,0 +1,6 @@
+class adminer::httpd::supervisor {
+  file { '/etc/supervisor/conf.d/httpd.conf':
+    ensure => present,
+    source => 'puppet:///modules/httpd/etc/supervisor/conf.d/httpd.conf'
+  }
+}
---
+++ src/build/modules/adminer/manifests/packages.pp
@@ -0,0 +1,12 @@
+class httpd::packages {
+  exec { 'apt-get update':
+    path => ['/usr/bin']
+  }
+
+  package {[
+      'apache2'
+    ]:
+    ensure => present,
+    require => Exec['apt-get update']
+  }
+}
