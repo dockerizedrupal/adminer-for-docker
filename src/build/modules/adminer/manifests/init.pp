123b2c163f0fc8e645fc3c3d823dc046adb10dac
Jürgen Viljaste <j.viljaste@gmail.com>
2014-11-18 19:53:39 +0200

    Update

--- src/build/modules/adminer/manifests/init.pp
+++ src/build/modules/adminer/manifests/init.pp
@@ -1,6 +1,11 @@
 class adminer {
   require adminer::httpd
 
+  file { '/var/www/adminer-4.1.0.php':
+    ensure => present,
+    source => 'puppet:///modules/adminer/var/www/adminer-4.1.0.php'
+  }
+
   file { '/var/www/index.php':
     ensure => present,
     source => 'puppet:///modules/adminer/var/www/index.php'
