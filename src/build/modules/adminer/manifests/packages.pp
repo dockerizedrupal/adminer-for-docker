65abdc2c1e0782a6bc475c1d3ea68bf1bd618ea2
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-07 20:38:25 +0200

    Update

--- src/build/modules/adminer/manifests/packages.pp
+++ src/build/modules/adminer/manifests/packages.pp
@@ -1,13 +1,8 @@
 class adminer::packages {
-  exec { 'apt-get update':
-    path => ['/usr/bin']
-  }
-
   package {[
       'php5',
       'php5-mysql'
     ]:
-    ensure => present,
-    require => Exec['apt-get update']
+    ensure => present
   }
 }
