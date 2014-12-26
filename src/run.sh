4dd487b4aa69824a6ee3227772e86553e2693d7a
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-26 22:00:35 +0200

    Update

--- src/run.sh
+++ src/run.sh
@@ -5,7 +5,16 @@ MYSQLD_PORT="$(echo "${MYSQLD_PORT}" | sed 's/tcp:\/\///')"
 export FACTER_MYSQLD_HOST="$(echo "${MYSQLD_PORT}" | cut -d ":" -f1)"
 export FACTER_MYSQLD_PORT="$(echo "${MYSQLD_PORT}" | cut -d ":" -f2)"
 
+if [ -z "${MYSQLD_USERNAME}" ]; then
+  MYSQLD_USERNAME="root"
+fi
+
 export FACTER_MYSQLD_USERNAME="${MYSQLD_USERNAME}"
+
+if [ -z "${MYSQLD_PASSWORD}" ]; then
+  MYSQLD_PASSWORD="root"
+fi
+
 export FACTER_MYSQLD_PASSWORD="${MYSQLD_PASSWORD}"
 
 if [ -z "${SERVER_NAME}" ]; then
