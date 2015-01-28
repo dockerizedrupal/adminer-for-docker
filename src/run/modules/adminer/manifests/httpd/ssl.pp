3d3b3f163664f793dfa31f5479425c5e09550f79
Jürgen Viljaste <j.viljaste@gmail.com>
2015-01-28 21:24:33 +0200

    Update

--- src/run/modules/adminer/manifests/httpd/ssl.pp
+++ src/run/modules/adminer/manifests/httpd/ssl.pp
@@ -1,10 +1,31 @@
-class adminer::httpd::ssl {
-  exec { 'openssl genrsa -out /adminer/ssl/private/adminerCA.key 4096':
-    timeout => 0,
-    path => ['/usr/bin']
+class adminer::adminer::ssl {
+  exec { 'mkdir -p /adminer/ssl':
+    path => ['/bin']
   }
 
-  exec { "openssl req -x509 -new -nodes -key /adminer/ssl/private/adminerCA.key -days 365 -subj /C=/ST=/L=/O=/CN=adminer -out /adminer/ssl/certs/adminerCA.crt":
+  exec { 'mkdir -p /adminer/ssl/private':
+    path => ['/bin'],
+    require => Exec['mkdir -p /adminer/ssl']
+  }
+
+  exec { 'mkdir -p /adminer/ssl/certs':
+    path => ['/bin'],
+    require => Exec['mkdir -p /adminer/ssl/private']
+  }
+
+  file { '/root/opensslCA.cnf':
+    ensure => present,
+    content => template('adminer/opensslCA.cnf.erb'),
+    require => Exec['mkdir -p /adminer/ssl/certs']
+  }
+
+  exec { 'openssl genrsa -out /adminer/ssl/private/adminerCA.key 4096':
+    timeout => 0,
+    path => ['/usr/bin'],
+    require => File['/root/opensslCA.cnf']
+  }
+
+  exec { "openssl req -sha256 -x509 -new -days 3650 -extensions v3_ca -config /root/opensslCA.cnf -key /adminer/ssl/private/adminerCA.key -out /adminer/ssl/certs/adminerCA.crt":
     timeout => 0,
     path => ['/usr/bin'],
     require => Exec['openssl genrsa -out /adminer/ssl/private/adminerCA.key 4096']
@@ -13,20 +34,24 @@ class adminer::httpd::ssl {
   exec { 'openssl genrsa -out /adminer/ssl/private/adminer.key 4096':
     timeout => 0,
     path => ['/usr/bin'],
-    require => Exec["openssl req -x509 -new -nodes -key /adminer/ssl/private/adminerCA.key -days 365 -subj /C=/ST=/L=/O=/CN=adminer -out /adminer/ssl/certs/adminerCA.crt"]
+    require => Exec["openssl req -sha256 -x509 -new -days 3650 -extensions v3_ca -config /root/opensslCA.cnf -key /adminer/ssl/private/adminerCA.key -out /adminer/ssl/certs/adminerCA.crt"]
   }
 
-  $subj = "/C=/ST=/L=/O=/CN=$server_name"
-
-  exec { "openssl req -sha256 -new -key /adminer/ssl/private/adminer.key -subj $subj -out /adminer/ssl/certs/adminer.csr":
-    timeout => 0,
-    path => ['/usr/bin'],
+  file { '/root/openssl.cnf':
+    ensure => present,
+    content => template('adminer/openssl.cnf.erb'),
     require => Exec['openssl genrsa -out /adminer/ssl/private/adminer.key 4096']
   }
 
-  exec { "openssl x509 -req -in /adminer/ssl/certs/adminer.csr -CA /adminer/ssl/certs/adminerCA.crt -CAkey /adminer/ssl/private/adminerCA.key -CAcreateserial -out /adminer/ssl/certs/adminer.crt -days 365":
+  exec { "openssl req -sha256 -new -config /root/openssl.cnf -key /adminer/ssl/private/adminer.key -out /adminer/ssl/certs/adminer.csr":
     timeout => 0,
     path => ['/usr/bin'],
-    require => Exec["openssl req -sha256 -new -key /adminer/ssl/private/adminer.key -subj $subj -out /adminer/ssl/certs/adminer.csr"]
+    require => File['/root/openssl.cnf']
+  }
+
+  exec { "openssl x509 -req -sha256 -CAcreateserial -days 3650 -extensions v3_req -extfile /root/opensslCA.cnf -in /adminer/ssl/certs/adminer.csr -CA /adminer/ssl/certs/adminerCA.crt -CAkey /adminer/ssl/private/adminerCA.key -out /adminer/ssl/certs/adminer.crt":
+    timeout => 0,
+    path => ['/usr/bin'],
+    require => Exec["openssl req -sha256 -new -config /root/openssl.cnf -key /adminer/ssl/private/adminer.key -out /adminer/ssl/certs/adminer.csr"]
   }
 }
