0e59d7b360c8391c67401236013433edc581fc81
Jürgen Viljaste <j.viljaste@gmail.com>
2015-09-01 17:37:34 +0300

    Update

--- src/adminer/variables.sh
+++ src/adminer/variables.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
-MYSQL_PORT="$(echo "${MYSQL_PORT}" | sed 's/tcp:\/\///')"
+if [ -n "${MYSQL_HOST}" ]; then
+  export FACTER_MYSQL_HOST="${MYSQL_HOST}"
 
-export FACTER_MYSQL_HOST="$(echo "${MYSQL_PORT}" | cut -d ":" -f1)"
-export FACTER_MYSQL_PORT="$(echo "${MYSQL_PORT}" | cut -d ":" -f2)"
+  if [ -z "${MYSQL_PORT}" ]; then
+    MYSQL_PORT="3306"
+  fi
+
+  export FACTER_MYSQL_PORT="${MYSQL_PORT}"
+else
+  MYSQL_PORT="$(echo "${MYSQL_PORT}" | sed 's/tcp:\/\///')"
+
+  export FACTER_MYSQL_HOST="$(echo "${MYSQL_PORT}" | cut -d ":" -f1)"
+  export FACTER_MYSQL_PORT="$(echo "${MYSQL_PORT}" | cut -d ":" -f2)"
+fi
 
 if [ -z "${MYSQL_USERNAME}" ]; then
   MYSQL_USERNAME="root"
